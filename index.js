8f8cbb6a1b8c9bf0cf3ba7ea7ebbaf3774636852
Kendra <klnbianchi@gmail.com>
2021-10-06 20:59:26 -0700

    finshed server part2. login/register/tokens

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
 
 const apiRouter = require('./api');
-const bodyParser = require('body-parser');
-server.use(bodyParser.json());
+
+server.use(express.json());
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
