8c72e78cbdd5ce9883f1bc0a0527678cfe78a4aa
Kendra <klnbianchi@gmail.com>
2021-10-06 19:52:50 -0700

    finshed part 1 of server. routes, middleware

--- db/seed.js
+++ db/seed.js
@@ -101,36 +101,35 @@ async function createInitialUsers() {
 
 async function createInitialPosts() {
     try {
-        const [albert, sandra, glamgal] = await getAllUsers();
-
-        console.log("Starting to create posts...");
-        await createPost({
-            authorId: albert.id,
-            title: "First Post",
-            content:
-                "This is my first post. I hope I love writing blogs as much as I love writing them.",
-            tags: ["#happy", "#youcandoanything"],
-        });
-
-        await createPost({
-            authorId: sandra.id,
-            title: "How does this work?",
-            content: "Seriously, does this even do anything?",
-            tags: ["#happy", "#worst-day-ever"],
-        });
-
-        await createPost({
-            authorId: glamgal.id,
-            title: "Living the Glam Life",
-            content: "Do you even? I swear that half of you are posing.",
-            tags: ["#happy", "#youcandoanything", "#canmandoeverything"],
-        });
-        console.log("Finished creating posts!");
+      const [albert, sandra, glamgal] = await getAllUsers();
+  
+      console.log("Starting to create posts...");
+      await createPost({
+        authorId: albert.id,
+        title: "First Post",
+        content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+        tags: ["#happy", "#youcandoanything"]
+      });
+  
+      await createPost({
+        authorId: sandra.id,
+        title: "How does this work?",
+        content: "Seriously, does this even do anything?",
+        tags: ["#happy", "#worst-day-ever"]
+      });
+  
+      await createPost({
+        authorId: glamgal.id,
+        title: "Living the Glam Life",
+        content: "Do you even? I swear that half of you are posing.",
+        tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+      });
+      console.log("Finished creating posts!");
     } catch (error) {
-        console.log("Error creating posts!");
-        throw error;
+      console.log("Error creating posts!");
+      throw error;
     }
-}
+  }
 
 async function testDB() {
     try {
