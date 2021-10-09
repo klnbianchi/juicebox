7b8a79dcebc31a4e9da8e9cab783c495088fee2a
Kendra <klnbianchi@gmail.com>
2021-10-09 11:53:27 -0700

    finsihed part 3. post, patch, delete routes

--- api/tags.js
+++ api/tags.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
-    console.log("A request is being made to /tags");
-  
+  console.log("A request is being made to /tags");
+
   next();
+});
+
+tagsRouter.get('/', async (req, res) => {
+  const tags = await getAllTags();
+
+  res.send({
+    "tags": tags
   });
+});
 
-  tagsRouter.get('/', async (req,res)=>{
-    const tags = await getAllTags();
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const { tagName } = req.params;
+  try {
+    const allPostTags = await getPostsByTagName(tagName);
 
-    res.send({
-       "tags":tags
+    const postTags = allPostTags.filter(post => {
+      if (post.active || (req.user && post.author.id === req.user.id)) {
+        return true;
+      }
+      return false;
     });
+    res.send({ posts: postTags });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
+
--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
 const { getAllUsers, getUserByUsername, createUser } = require('../db');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 usersRouter.use((req, res, next) => {
     console.log("A request is being made to /users");
